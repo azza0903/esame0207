0fa4d8ded73ef100de59c022e9ade2bb82d66f0e
azza0903 <azza.bennaceur@edu.unifi.it>
2025-06-28 02:54:48 +0000

    agg CMakeListes.txt per eseg DiarioAttivita

---
+++ build/CMakeFiles/DiarioAttivita.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiarioAttivita.dir/DiarioAttivita.cpp.o"
+  "CMakeFiles/DiarioAttivita.dir/DiarioAttivita.cpp.o.d"
+  "CMakeFiles/DiarioAttivita.dir/main.cpp.o"
+  "CMakeFiles/DiarioAttivita.dir/main.cpp.o.d"
+  "DiarioAttivita"
+  "DiarioAttivita.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DiarioAttivita.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/DiarioAttivita.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for DiarioAttivita.
+# This may be replaced when dependencies are built.
